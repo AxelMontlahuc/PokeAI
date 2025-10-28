4bec93e30d21deed1ba8af57139de702e0df0117
AxelMontlahuc <axel.montlahuc@proton.me>
2025-10-28 15:36:57 +0100

    Updated state fetching method

--- src/state.h
+++ src/state.h
@@ -7,9 +7,7 @@
 #include <math.h>
 #include <assert.h>
 
-#include "../mGBA-interface/include/mgba_connection.h"
-#include "../mGBA-interface/include/mgba_controller.h"
-#include "../mGBA-interface/include/mgba_intel.h"
+#include "../gba/gba.h"
 
 #define typeshit typedef
 
@@ -37,39 +35,13 @@ typeshit struct state {
 typeshit struct trajectory {
     state* states;
     double** probs;
-    MGBAButton* actions;
+    int* actions;
     double* rewards;
     double* values;
     int steps;
 } trajectory;
 
-static inline int actionToIndex(MGBAButton action) {
-    switch (action) {
-        case MGBA_BUTTON_UP: return 0;
-        case MGBA_BUTTON_DOWN: return 1;
-        case MGBA_BUTTON_LEFT: return 2;
-        case MGBA_BUTTON_RIGHT: return 3;
-        case MGBA_BUTTON_A: return 4;
-        case MGBA_BUTTON_B: return 5;
-        case MGBA_BUTTON_START: return 6;
-        default: return 5;
-    }
-}
-
-static inline MGBAButton indexToAction(int index) {
-    switch (index) {
-        case 0: return MGBA_BUTTON_UP;
-        case 1: return MGBA_BUTTON_DOWN;
-        case 2: return MGBA_BUTTON_LEFT;
-        case 3: return MGBA_BUTTON_RIGHT;
-        case 4: return MGBA_BUTTON_A;
-        case 5: return MGBA_BUTTON_B;
-        case 6: return MGBA_BUTTON_START;
-        default: return MGBA_BUTTON_B;
-    }
-}
-
-state fetchState(MGBAConnection conn);
+state fetchState();
 void convertState(state s, double* out);
 trajectory* initTrajectory(int steps);
 void freeTrajectory(trajectory* traj);
